b77f132111c63b980a1e9f48e9c864c8748dbc67
Pruthvishankar3 <pruthvishankar877@gmail.com>
2026-03-02 21:07:13 +0530

    square and cube.c

---
+++ square and cube.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,Square,Cube;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    Square=a*a;
+    Cube=a*a*a;
+    printf("Square:%d\n",Square);
+    printf("Cube:%d",Cube);
+    return 0;
+}
